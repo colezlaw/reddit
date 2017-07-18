2d780f2b5c16151d29091c1dd245b1d6ca63804c
colezlaw <will@cltnc.us>
2017-07-18 13:35:02 -0400

    geddit allows specifying the subreddit

--- geddit/main.go
+++ geddit/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/colezlaw/reddit"
 )
 
+var subreddit string
+
+func init() {
+	flag.StringVar(&subreddit, "s", "golang", "The subreddit to seatch")
+}
+
 func main() {
-	items, err := reddit.Get("golang")
+	flag.Parse()
+	items, err := reddit.Get(subreddit)
 	if err != nil {
 		log.Fatal(err)
 	}
